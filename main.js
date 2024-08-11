99dddf1ca399753cfe9bfcc77339ef04f27153f6
Miguel <delgdevoleper@gmail.com>
2024-08-11 12:34:59 -0300

    Desestruturação

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-//Nullesh Coalesing Operator -> operador para lidar com nulos
+// Objetos
 
-const idade = 0;
+const user = {
+  name: 'Miguel',
+  idade: 20,
+  nickname: 'Malucao',
+  address: {
+    street: 'Rua Teste',
+    number: 107
+  }
+};
 
-// 0, '', [], false, undefined, null => falsy 
-// o ?? serve para separar valores que sao significativos pros que nao sao.
-// 0 é um valor significativo, se eu usar || o javascrip vai identificar como
-// nao significativo, já usando o operador ?? o javcascript identifica 
-// como significativo.
 
-document.body.innerText = 'Sua idade é: ' + (idade ?? 'Não informado');
+
+// Desestruturação -> remoção de parte do objeto para uma váriavel.
+// Atribuindo um valor default no nickname, mas se esse atributo existir no objeto, a preferencia de valor é do objeto.
+const { address, idade: age, nickname = 'Miguelzera' } = user
+
+function mostraIdade({ name, idade }) {
+  return name + " " + idade
+}
+
+document.body.innerText = mostraIdade(user)
+
+/*o operador in serve para verificar se há um atributo dentro do objeto user
+document.body.innerText = ('name' in user)
+
+Retorna um array com todos os atributos presentes no objeto user
+document.body.innerText = Object.keys(user)
+
+Retorna os valores dos atributos
+document.body.innerText = Object.values(user)
+
+Retorna os atributos e seus valores em formato json.
+document.body.innerText = JSON.stringify(Object.values(user))
+
+Retorna um vetor com o objeto e seus valores. 
+document.body.innerText = JSON.stringify(Object.entries(user)) */
+
+
+
+
+
+
+
