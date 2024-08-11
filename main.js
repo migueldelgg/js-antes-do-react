0849eaa7f5f6dcbe89196d99482100b4ce188603
Miguel <delgdevoleper@gmail.com>
2024-08-11 12:45:32 -0300

    Short Sintax

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-// Objetos
+
+// Short sintax
+
+const name ='Miguel'
+
+const age = 20
 
 const user = {
-  name: 'Miguel',
-  idade: 20,
-  nickname: 'Malucao',
-  address: {
-    street: 'Rua Teste',
-    number: 107
-  }
-};
-
-// Desestruturação
-
-function mostraIdade({ name, idade }) {
-  return name + " " + idade
+  name, 
+  age
 }
 
-// Rest Operator -> operador de resto.
-
-//const {name, idade, ...rest } = user;
-
-const array = [1,2,3,4,5,6,7,8,9,10]
-
-// Pulando o second do array.
-const [first, , third, ...rest] = array;
-
-document.body.innerText = JSON.stringify({first, third, rest})
+document.body.innerText = JSON.stringify({user})
 
 
 
