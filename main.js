13336c8700deecd300c74826d646cded45cb6a42
Miguel <delgdevoleper@gmail.com>
2024-08-11 17:35:30 -0300

    importando *, renomeando importacoes

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-import { SOMAR } from "./lib/math"
-import { SUBTRAIR } from "./lib/math"
-import somadinha from "./lib/sum"
+//import * as math from './lib/math'
 
-console.log(SOMAR(2,2))
-console.log(SUBTRAIR(4,2))
-console.log(somadinha(2,2))
-// Named Export
+import { SOMAR as sominha} from './lib/math'
+
+console.log(sominha(1,2))
