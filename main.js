a14362448f97f6d12899863c58c346b4c2cff592
Miguel <delgdevoleper@gmail.com>
2024-08-11 14:35:25 -0300

    manipulacao de array com filter e map

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 
-// Optional Chaining
+const array = [1, 2, 3, 4, 5]
 
-const user = {
-  //name: 'Miguel',
-  age: 20,
-  address: {
-    street: 'Rua Teste',
-    number: 107,
-    zip: {
-      code: 895500,
-      city: 'Guarulhos'
-    },
-    //showFullAdress(){
-    //  return 'ok';
-    //}
-  }
-};
+//for(const i of array){
+// document.body.innerText += i
+//}
 
-const key = 'state';
+//array.forEach(i => {
+//  document.body.innerText += i
+//})
 
-document.body.innerText = user.address[key] ?? 'nao existe'
+
+// Map sempre vai retornar um array do mesmo tamanho do array original
+// usamos o map() quando queremos transformar o array antigo em algo novo, porem de mesmo tamanho.
+// const novoArray = array.map(item => {
+//   if(item % 2 === 0){
+//     return item * 10
+//   }
+//   return item
+// })
+
+// map, filter, every, some, find, findIndex, reduce
+
+// const novoArray = array.filter(item => item % 2 === 0)
+//   .map(item => item * 10)
+
+const novoArray = 
+
+document.body.innerText = JSON.stringify(novoArray)
+
+
+
 
 
 
