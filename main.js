6729cdf4fe84f945e9f5f0a11a6c96f3a5e4295d
Miguel <delgdevoleper@gmail.com>
2024-08-11 17:08:17 -0300

    promises pt 2, promessas alinhadas

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 // Promisses
 // Existem funções que demoram para retornam informações
 
-
-const somaDoisNumeros = (a, b) => {
-  return new Promise ((resolve, reject) => {
-    setTimeout(() => {
-      resolve(a+ b)
-    }, 2000)
-  });
-}
-
-somaDoisNumeros(1,2)
-  .then(soma => {
-    document.body.innerText  = soma
+fetch('https://api.github.com/users/migueldelgg')
+  .then(response => {
+    return response.json();
   })
+  .then(body => {
+    console.log(body)
+  })
+  // .then(response => {
+  //   response.json().then(body => {
+  //     console.log(body)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // })
   .catch(err => {
     console.log(err)
   })
+  .finally(() => {
+    console.log('finalmente!')
+  })
+
